ccf00c39060f9a62d37e6f50fa9927f4a4405dd9
Tanisha jain <109541369+jaintanisha0126@users.noreply.github.com>
2024-05-26 12:45:33 +0530

    Add files via upload

---
+++ atm project with java/atm.java
@@ -0,0 +1,36 @@
+
+public class atm {
+    private double balance;
+    private double depositAmount;
+    private double withdrawAmount;
+
+    //default constructor
+    public atm(){
+
+    }
+
+    //getter setter
+public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(double depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    public double getWithdrawAmount() {
+        return withdrawAmount;
+    }
+
+    public void setWithdrawAmount(double withdrawAmount) {
+        this.withdrawAmount = withdrawAmount;
+    }
+}
